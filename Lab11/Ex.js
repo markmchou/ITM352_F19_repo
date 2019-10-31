d652ade4d3b1577b1d7c5b715ecb7a382324975f
Mark Chou <markchou@hawaii.edu>
2019-10-31 00:51:37 -1000

    Labs

---
+++ Lab11/Ex.js
@@ -0,0 +1,28 @@
+
+attributes = "Mark;20;20.5;-19.5";
+theSeparator = ';';
+parts = attributes.split(theSeparator);
+
+//parts =['Mark',20,20.5,-19.5]
+
+//for (i=0; i < parts.length; i++){
+//console.log(`${parts[i]} isNotNegInt ${isNotNegInt(parts[i],true)}`);
+parts.forEach(function (item, index) {
+    console.log((typeof item == 'string' && item.length > 0) ? true : false);
+}
+//}
+
+function printIt(item, index) {
+        console.log(`${item} isNotNegInt ${isNotNegInt(item, true)}`);
+    }
+
+console.log(parts.join(theSeparator))
+
+function isNotNegInt(q, returnErrors = false) {
+        errors = []; // assume no errors at first
+        if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+        if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+        if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+        return returnErrors ? errors : (errors.length == 0);
+    }
+console.log(isNotNegInt(3));
