2380552010dd6d96b9e9327b6c936a2fc41be36a
Mark Chou <markchou@hawaii.edu>
2019-11-12 12:48:15 -1000

    Assignment

---
+++ Chou_Mark_Assignment1/public/product_data.js
@@ -0,0 +1,31 @@
+var products = 
+[
+    {
+        "model": "Poke Ball",
+        "price": 10.00,
+        "image": "PokeBall.png"
+    },
+    {
+        "model": "Great Ball",
+        "price": 20.00,
+        "image": "GreatBall.png"
+    },
+    {
+        "model": "Ultra Ball",
+        "price": 30.00,
+        "image": "UltraBall.png"
+    },
+    {
+        "model": "Master Ball",
+        "price": 40.00,
+        "image": "MasterBall.png"
+    },
+    {
+        "model": "Poke Balls Combo",
+        "price": 40.00,
+        "image": "PokeBallsCombo.png"
+    }
+]
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
---
+++ Chou_Mark_Assignment1/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var myParser = require("body-parser");
+var data = require("./public/product_data.js");
+
+var app = express();
+app.all('*', function (request, response, next) {
+   console.log(request.method + ' to ' + request.path);
+   next();
+});
+
+app.use(myParser.urlencoded({ extended: true }));
+app.post("/process_form", function (request, response) {
+   let POST = request.body;
+   if (typeof POST['quantity_textbox'] != 'undefined') { displayPurchase(POST, response) }
+});
+
+app.use(express.static('./public'));
+app.listen(8080, () => console.log(`listening on port 8080`));
